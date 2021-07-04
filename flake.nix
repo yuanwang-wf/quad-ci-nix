579dee37fa476f64866a8925176c2d33d982ca31
yuanwang <yuan.wang@workiva.com>
2021-07-04 07:49:15 -0600

    init project

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Walkthrough of The Simple Haskell Handbook";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils/master";
+    devshell.url = "github:numtide/devshell/master";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, devshell }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlay = final: prev: { };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlay overlay ];
+        };
+
+        myHaskellEnv = (pkgs.haskellPackages.ghcWithHoogle (p:
+          with p; [
+            cabal-install
+            ormolu
+            hlint
+            brittany
+            record-dot-preprocessor
+          ]));
+
+      in rec {
+
+        defaultPackage = pkgs.hello;
+        devShell = pkgs.devshell.mkShell {
+          name = "dev-shell";
+          env = [
+            {
+              name = "HIE_HOOGLE_DATABASE";
+              value = "${myHaskellEnv}/share/doc/hoogle/default.hoo";
+            }
+            {
+              name = "NIX_GHC";
+              value = "${myHaskellEnv}/bin/ghc";
+            }
+            {
+              name = "NIX_GHCPKG";
+              value = "${myHaskellEnv}/bin/ghc-pkg";
+            }
+          ];
+          packages = [ myHaskellEnv pkgs.nixpkgs-fmt pkgs.hpack ];
+        };
+      });
+}
